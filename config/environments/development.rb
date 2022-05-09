5bc38a302005b201aa5618ccf11136967612dbb6
tacchan7216 <ec2-user@ip-172-31-47-4.ec2.internal>
2022-05-09 07:57:32 +0000

    [Finish]2-2

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,4 +73,7 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+  
+  config.hosts << "1f66de3a73664cc49f1420a5594a00ec.vfs.cloud9.us-east-1.amazonaws.com"
+  
 end
